0c3274808eee574447b2c7c219af7f99e39ea905
Aaryan Patel <aaryan_20061@ldrp.ac.in>
2023-12-19 12:16:14 +0530

    price component created

--- src/components/products/ProductCard/index.tsx
+++ src/components/products/ProductCard/index.tsx
@@ -2,29 +2,30 @@ import { ProductCardType } from "@/utils/types";
 import Image from "next/image";
 import Styles from "./productCard.module.css";
 import AddToCartBtn from "../AddToCartBtn";
+import Price from "@/components/Price";
 
 const ProductCard = ({ data }: ProductCardType) => {
-	return (
-		<div className={Styles.main}>
-			<div className={Styles.productWrapper}>
-				<div className={Styles.productImage}>
-					<Image
-						src={data.productThumbnail.url}
-						alt={data.productName}
-						fill
-						style={{ objectFit: "contain" }}
-					/>
-				</div>
-				<div className={Styles.content}>
-					<p className={Styles.productName}>{data.productName}</p>
-					<p className={Styles.productPrice}>{data.productPrice}</p>
-				</div>
-			</div>
-			<div className={Styles.btnWrapper}>
-				<AddToCartBtn varient="icon" />
-			</div>
-		</div>
-	);
+  return (
+    <div className={Styles.main}>
+      <div className={Styles.productWrapper}>
+        <div className={Styles.productImage}>
+          <Image
+            src={data.productThumbnail.url}
+            alt={data.productName}
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+        <div className={Styles.content}>
+          <p className={Styles.productName}>{data.productName}</p>
+          <Price price={data.price} />
+        </div>
+      </div>
+      <div className={Styles.btnWrapper}>
+        <AddToCartBtn varient="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
